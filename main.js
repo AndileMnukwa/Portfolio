c7e0c622e0b411892cef753d347e118051b2ae93
AndileMnukwa <mnukwa1996@gmail.com>
2024-02-29 13:28:09 +0200

    Home

--- main.js
+++
@@ -1,7 +0,0 @@
-// Header Toggle
-let MenuBtn = document.getElementById('MenuBtn')
-
-MenuBtn.addEventListener('click',function(e){
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    this.classList.toggle('fa-xmark')
-})
